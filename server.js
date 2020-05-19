a5cd8ec3b98eed3f6f2fcd7d2c81e993b6b04b21
Arindam Lahiri <arindamlahiri17@gmail.com>
2020-05-20 05:06:25 +0530

    section 7 started

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
-const validator = require('express-validator')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 const mongodb = require('mongodb')
 const mongoClient = mongodb.MongoClient
 
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended:false }))
-app.use(express.json())
+app.use(express.json({ extended:false }))
 
 app.get('/',(req,res) => {
     res.json({msg:"hello from server"})
