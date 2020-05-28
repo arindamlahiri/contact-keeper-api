c66acacf8e3e17accdec9fb82ce620a4927f8ad1
Arindam Lahiri <54589973+arindamlahiri@users.noreply.github.com>
2020-05-29 01:44:11 +0530

    changes made for deployment

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require('express')
 const app = express()
 const mongodb = require('mongodb')
 const mongoClient = mongodb.MongoClient
+const path = require('path')
 
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended:false }))
 app.use(express.json({ extended:false }))
 
-app.get('/',(req,res) => {
-    res.json({msg:"hello from server"})
-})
-
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 var PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
