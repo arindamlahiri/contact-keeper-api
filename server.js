7aea1bb9e95be28bb0dca5a3ea422b9f4682234e
Arindam Lahiri <54589973+arindamlahiri@users.noreply.github.com>
2020-05-30 01:25:23 +0530

    prevented clickjacking

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use('/api/contacts', require('./routes/contacts'))
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+    app.get('*', (req,res) => {
+        res.setHeader("X-Frame-Options", "DENY");
+        res.setHeader("Content-Security-Policy", "frame-ancestors 'none'");
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 var PORT = process.env.PORT || 5000
